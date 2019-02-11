c136acc48d1560b62f364139d5d5480c7f208b9a
lukasribeiro <luckas.ribeiro2015@gmail.com>
2019-02-11 19:27:45 -0200

    usando express

---
+++ cap9 - express/app.js
@@ -0,0 +1,61 @@
+
+var express = require('express');
+var app = express();
+
+
+// Desliga o cache do express.
+app.disable('etag');
+
+
+// Configura para ler parâmetros por form-urlencoded e application/json
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// lendo parametros por GET do tipo Query Parameters
+app.get('/pessoa', function (req, res) {
+	let nome = req.query.nome;
+	let sobrenome = req.query.sobrenome;
+	res.status(200).type("text");
+	res.send(nome + " " + sobrenome);
+})
+
+
+// POST em /pessoa
+app.post('/pessoa', function (req, res) {
+	let nome = req.body.nome;
+	let sobrenome = req.body.sobrenome;
+	// Testa o valor do cabeçalho content-type
+	if(req.is("json")) {
+		// Se for JSON
+		res.json( { nome:nome, sobrenome:sobrenome } );
+	} else {
+		// Caso contrário envia como texto
+		res.type("text").send("Texto: " + nome + " " + sobrenome);
+	}
+})
+///////////////////////////////////////////////////////////////////////////////
+
+// lendo parametros por GET do tipo Path Parameters
+app.get('/pessoa/:id', function (req, res) {
+	let id = req.params.id;
+	res.send("Buscar a Pessoa: " + id);
+
+})
+
+app.get('/pessoa/nome/:nome/sobrenome/:sobrenome', function (req, res) {
+	let nome = req.params.nome;
+	let sobrenome = req.params.sobrenome;
+	res.send(nome + " " + sobrenome);
+})
+
+setTimeout(function () {
+  console.log('boo')
+}, 100)
+
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Servidor iniciado em http://%s:%s", host, port)
+})
